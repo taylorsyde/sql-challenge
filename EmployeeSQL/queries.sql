7683caca8048b09b1e768fdb07de067567ef6271
taylorsyde <tay.lyons@hotmail.com>
2021-08-29 18:29:27 -0500

    schema updated

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,15 @@
+-- list the employee first and last name, sex, and salary
+
+-- list the first and last name of employees hired in 1986
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
--- EmployeeSQL/schemas.sql
+++ EmployeeSQL/schemas.sql
@@ -1,29 +1,22 @@
 DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
-
 CREATE TABLE departments (
 	dept_no varchar(10) NOT NULL,
-	dept_name varchar(50) NOT NULL
+	dept_name varchar(50) NOT NULL,
+	primary key (dept_no)	
 );
+SELECT * from departments;
 
-CREATE TABLE dept_emp (
-	emp_no int NOT NULL,
-	dept_no varchar(10) NOT NULL
+
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles (
+	title_id varchar(10),
+	title varchar(50),
+	primary key (title_id)
 );
+SELECT * from titles;
 
--- column dept_mngt was added to this table to differentiate 
--- these employees from the other employees
--- this may need to be changed
-CREATE TABLE dept_manager (
-	dept_no varchar(10) NOT NULL,
-	emp_no int NOT NULL,
-	dept_mngt boolean
-);
 
+DROP TABLE IF EXISTS employees;
 -- emp_title was changed to title id to match the titles cvs
 CREATE TABLE employees (
 	emp_no int not null,
@@ -36,28 +29,37 @@ CREATE TABLE employees (
     primary key (emp_no),
     foreign key (title_id) REFERENCES titles (title_id)
 );
+SELECT * from employees;
 
+
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp (
+	emp_no int NOT NULL,
+	dept_no varchar(10) NOT NULL,
+	primary key (emp_no, dept_no),
+	foreign key (emp_no) REFERENCES employees (emp_no),
+	foreign key (dept_no) REFERENCES departments (dept_no)
+);
+SELECT * from dept_emp;
+
+
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager (
+	dept_no varchar(10) NOT NULL,
+	emp_no int NOT NULL,
+	primary key (dept_no, emp_no),
+	foreign key (emp_no) REFERENCES employees (emp_no),
+	foreign key (dept_no) REFERENCES departments (dept_no)
+);
+SELECT * from dept_manager;
+
+DROP TABLE IF EXISTS salaries;
 CREATE TABLE salaries (
 	emp_no int not null,
-	salary int not null
+	salary int not null,
+	primary key (emp_no,),
+	foreign key (emp_no) REFERENCES employees (emp_no)
 );
-	
-CREATE TABLE titles (
-	title_id varchar(10),
-	title varchar(50),
-	primary key (title_id)
-);
+SELECT * from salaries;
 
 -- import data
-
--- set all dept_mgnt to true
-update dept_manager 
-set dept_mngt = true;
-
--- verify that data was imported
-SELECT * FROM departments
-SELECT * FROM dept_emp
-SELECT * FROM dept_manager
-SELECT * FROM employees
-SELECT * FROM salaries 
-SELECT * FROM titles 
